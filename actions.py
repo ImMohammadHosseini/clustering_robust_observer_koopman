4f3fdfb62138ce1e7039e08e7236aa62d2242921
ImMohammadHosseini <sayedmh50@gmail.com>
2025-01-27 12:51:40 +0330

    change clustering based on the calculating centers for all features, add computing phase for cluster in separate method, since we need phase for both data and clusters in future and calculating residuals

--- actions.py
+++ actions.py
@@ -20,6 +20,7 @@ import onesine
 import tf_cover
 from tslearn.utils import to_time_series_dataset
 from tslearn.clustering import TimeSeriesKMeans
+from tslearn.barycenters import dtw_barycenter_averaging
 
 # Number of training episodes
 N_TRAIN = 18
@@ -137,42 +138,143 @@ def action_preprocess_experiments(
 
 def action_one_step_DTW_K_means_clustering(
     dataset_path: pathlib.Path,
-    cluster_pred,
-    clusters,
+    clusters_path: pathlib.Path,
     k: int,
     features_to_cluster: list,  # List of features to cluster, e.g. ['joint_pos', 'joint_vel']
     
 ):
+    max_iter=3
     dataset = joblib.load(dataset_path)
+    time_series = ['joint_pos', 'joint_vel', 'joint_trq', 'target_joint_pos', 
+                   'target_joint_vel']
+    gp_dataset = dataset.groupby(by=["serial_no", "load", "episode"])
     wh_data = []
+    n_wh_data = []
+    other_center_parts = []
     for cluster_base in features_to_cluster:
+        ocp=list(set(time_series) - set(cluster_base))
+        other_center_parts.append(ocp)
         ts_data=[]
         for part in cluster_base:
             x=[]
-            for i, dataset_ep in dataset.groupby(by=["serial_no", "load", "episode"]):
+            for i, dataset_ep in gp_dataset:
                 x.append(dataset_ep[part].to_list())
             ts_data.append(to_time_series_dataset(x))
         wh_data.append(np.concatenate(ts_data,2))
-    
+        n_ts_data=[]
+        for part in ocp:
+            x=[]
+            for i, dataset_ep in gp_dataset:
+                x.append(dataset_ep[part].to_list())
+            ts_data.append(to_time_series_dataset(x))
+        n_wh_data.append(np.concatenate(n_ts_data,2))
     
 
-    km = TimeSeriesKMeans(n_clusters=k, verbose=True, max_iter=10, 
-                          metric='dtw', random_state=0, n_jobs=2)
-    y_preds_dict={}
-    centers_dict={}
-    for i, cluster_base in enumerate(wh_data):
-        y_pred=km.fit_predict(cluster_base)
-        for j in range(k):
-            centers_dict['cluster'+str(i)+'center'+str(j)]=[km.cluster_centers_[i]]
-        y_preds_dict['cluster'+str(i)]=y_pred
-
+    km = TimeSeriesKMeans(n_clusters=k, verbose=True, max_iter=max_iter, 
+                          metric='dtw', random_state=0, n_jobs=4)
+    #y_preds_dict={}
+    centers_dict={'k':[], 't':[], 'joint_pos':[], 'joint_vel':[], 'joint_trq':[], 
+                  'target_joint_pos':[], 'target_joint_vel':[], 'clustering_no':[],
+                  'center_no':[]}
+    for i, cluster_base in enumerate(zip(features_to_cluster, wh_data)):
+        
+        y_pred=km.fit_predict(cluster_base[1])
+        for center_num in range(k):
+            size=km.cluster_centers_[center_num].shape[0]
+            k=np.arange(0,size)
+            centers_dict['k'].extend(k)
+            t=k/1000
+            centers_dict['t'].extend(t)
+            centers_dict['clustering_no'].extend([i]*size)
+            centers_dict['center_no'].extend([center_num]*size)
+            for j, center_part_name in enumerate(cluster_base[0]):
+                centers_dict[center_part_name].extend(km.cluster_centers_[center_num][:,j])
+            for p, center_name in enumerate(other_center_parts[i]):
+                cl_mems = np.where(y_pred == center_num)[0]
+                centers_dict[center_name].extend(dtw_barycenter_averaging(
+                    n_wh_data[i][cl_mems][:,:,p], max_iter=max_iter)[:,0])
+                
     df = pandas.DataFrame(centers_dict)
-    joblib.dump(df, clusters)
-    df = pandas.DataFrame(y_preds_dict)
-    joblib.dump(df, y_preds_dict)
+    joblib.dump(df, clusters_path)
 
-
-
+def action_compute_cluster_phase(
+    clusters_path: pathlib.Path,
+    clusters_phase_path: pathlib.Path,
+):
+    clusters_phase_path.parent.mkdir(parents=True, exist_ok=True)
+    clusters = joblib.load(clusters_path)
+    n_phase_samples = 1000
+    min_length = 600
+    min_vel = 3
+    trim = 100
+    df_lst = []
+    for i, dataset_ep in clusters.groupby(by=["clustering_no","center_no"]):
+        tvel = dataset_ep["target_joint_vel"].to_numpy()
+        vel = dataset_ep["joint_vel"].to_numpy()
+        tpos = dataset_ep["target_joint_pos"].to_numpy()
+        pos = dataset_ep["joint_pos"].to_numpy()
+        # Find points where velocity changes
+        vel_changes = np.ravel(np.argwhere(np.diff(tvel, prepend=0) != 0))
+        # Split into constant-velocity segments
+        X = np.vstack(
+            [
+                pos,
+                vel,
+                tpos,
+                tvel,
+            ]
+        ).T
+        const_vel_segments = np.split(X, vel_changes)
+        # Find first segment of required length and speed
+        for segment in const_vel_segments:
+            X_const_vel = segment[trim:-trim, :]
+            if segment.shape[0] > min_length:
+                if np.all(segment[:, 3] > min_vel):
+                    direction = "forward"
+                elif np.all(segment[:, 3] < -1 * min_vel):
+                    direction = "reverse"
+                else:
+                    continue
+                # Compute normalized velocity error in that segment
+                vel_err = X_const_vel[:, 1] - X_const_vel[:, 3]
+                norm_vel_err = vel_err / np.max(np.abs(vel_err))
+                # Create array of phases to test
+                phases = np.linspace(0, 2 * np.pi, n_phase_samples)
+                # Compute inner product of error and shifted signal for each phase
+                inner_products = (
+                    np.array(
+                        [
+                            np.sum(norm_vel_err * np.sin(100 * X_const_vel[:, 0] + p))
+                            for p in phases
+                        ]
+                    )
+                    / norm_vel_err.shape[0]
+                )
+                # Find best phase
+                # There are two phases that will work (+ve and -ve correlations)
+                optimal_phase = phases[np.argmax(inner_products)]
+                df_lst.append(
+                    i + (direction, optimal_phase, phases, inner_products),
+                )
+    df = pandas.DataFrame(
+        df_lst,
+        columns=[
+            "serial_no",
+            "load",
+            "episode",
+            "direction",
+            "optimal_phase",
+            "phases",
+            "inner_products",
+        ],
+    )
+    df.sort_values(
+        by=["serial_no", "load", "episode"],
+        inplace=True,
+    )
+    joblib.dump(df, clusters_phase_path)
+    
+    
 def action_compute_phase(
     dataset_path: pathlib.Path,
     phase_path: pathlib.Path,
